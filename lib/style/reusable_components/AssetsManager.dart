a140bd3bc0fda49b1ce08ed632e156cd9867b0fa
amr ali <amrelhaj10@gmail.com>
2025-02-21 17:55:24 +0200

    +finshed sebha +add onboarding assets

--- lib/style/reusable_components/AssetsManager.dart
+++ lib/style/reusable_components/AssetsManager.dart
@@ -20,5 +20,12 @@ abstract final class Assetsmanager {
   static const String sura_number = "assets/icons/sura_number.svg";
   static const String time = "assets/icons/time.svg";
   static const String HadithCardBack = "assets/images/hadithCardBack.png";
+  static const String sebhaBody = "assets/images/SebhaBody.png";
+  static const String sebhaHead = "assets/images/sebhaHead.png";
+  static const String onBoarding1 = "assets/images/onBoarding1 (2).png";
+  static const String onBoarding2 = "assets/images/onBoarding2.png";
+  static const String onBoarding3 = "assets/images/onBoarding3.png";
+  static const String onBoarding4 = "assets/images/onBoarding4.png";
+  static const String onBoarding5 = "assets/images/onBoarding5.png";
 
 }
